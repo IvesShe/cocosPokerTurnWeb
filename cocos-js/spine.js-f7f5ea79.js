23d4819ed8278fe4d9da5a4bcbe35f3b3624d05d
TECIVESSHELAT54\IvesSheLAT5450 <ivesshe@gmail.com>
2024-06-21 15:58:09 +0800

    update

--- cocos-js/spine.js-f7f5ea79.js
+++ cocos-js/spine.js-f7f5ea79.js
@@ -1,11 +1,1 @@
-System.register([], (function (exports, module) {
-	'use strict';
-	return {
-		execute: (function () {
-
-			var spine_js = exports('default', 'assets/spine.js.mem-1ff7a7d3.bin');
-
-		})
-	};
-}));
-//# sourceMappingURL=spine.js-f7f5ea79.js.map
+System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-1ff7a7d3.bin")}}}));
